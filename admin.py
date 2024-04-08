1a2a0d7f3e2994edaa3905810a73b0bada5fab79
Nitin Venkat <85633289+Nitinvenkat27@users.noreply.github.com>
2024-04-08 16:50:59 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MessFeedback
-
-# Register your models here.
-class MessFeedbackAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date', 'type', 'rating', 'review')
-admin.site.register(MessFeedback, MessFeedbackAdmin)
